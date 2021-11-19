1f3ec4ff342d66c7b0de3d333e7383c3fc9658f0
Subhadeep <subhadeep0326@gmail.com>
2021-11-20 00:39:31 +0530

    [Subhadeep] add Quadratic Eqn root calc

---
+++ FunctionalPrograms/Distance.py
@@ -0,0 +1,25 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-11-19 14:35
+	@Title : Distance of a point from origin
+	
+'''
+
+def calc_distance(x, y):
+    """	
+	Description:
+	    This function enables us to calculate the distance of a point from origin
+	Parameter:
+	    Two integer values as the coordinates of the point
+	Return:
+	    The distance of the point from origin
+	"""
+    #the formula of calculating distance from origin is dist= sqrt((x1-0)**2 + (y1-0)**2)
+    dist = ((x*x)+(y*y))**0.5
+    return dist
+
+x1= int(input("Please enter x coordinate: "))
+y1= int(input("Please enter y coordinate: "))
+
+result = calc_distance(x1,y1)
+print("The distance from the origin is: ",result)
